535a4b60df96ad81047c121d70209285f0b14841
bmorse <morse.brad10@gmail.com>
2016-01-28 21:11:47 -0500

    week 2 exercise.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+      String[] weekDays = {"sun", "mon", "tues",
+              "wed", "thurs", "fri", "sat"};
+        int [] temps = {45, 29, 10, 22, 41, 28, 33};
+        int[] precip = {95, 60, 25, 5, 0, 75, 90};
+        for (int index = 0; index< weekDays.length; index++){
+            if((temps[index]<= 32)&&(precip[index]>50)){
+                System.out.println(weekDays[index]);
+            }
+        }
     }
 }
